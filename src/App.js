4aa11322d7ecf4819a365420dfed348cd5ff88d1
anujbhandariNL <anuj@neuroleapmail.com>
2024-01-24 16:49:24 -0800

    Initial commit of frontend code

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   const handleSubmit = async () => {
     setIsLoading(true);
     try {
-      const response = await fetch('https://aistockadvisor-api.onrender.com', { 
+      const response = await fetch('http://localhost:3001/get-recommendations', { 
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
